fb5431047b7447b0ac39622e69a1ded2079256f9
Victor Casanas <victor.casanas@moonvision.io>
2019-05-14 17:43:34 +0200

    Add missing CustomerProviderInterface.

--- Security/User/CustomerProvider.php
+++ Security/User/CustomerProvider.php
@@ -2,10 +2,10 @@
 
 namespace Moonshiner\BrigthenBundle\Security\User;
 
-use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
-use Pimcore\Model\User;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use CustomerManagementFrameworkBundle\CustomerProvider\CustomerProviderInterface;
 
 
 class CustomerProvider implements UserProviderInterface
