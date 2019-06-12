0f5c54a00234e838e9b2af3140673d6e0e85741d
Victor Casanas <victor.casanas@moonvision.io>
2019-06-12 20:48:26 +0200

    Add document aeraBricks.

---
+++ Document/Areabrick/AbstractAreabrick.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Moonshiner\BrigthenBundle\Document\Areabrick;
+
+use Pimcore\Extension\Document\Areabrick\AbstractTemplateAreabrick;
+use Pimcore\Model\Document\Tag\Area\Info;
+
+abstract class AbstractAreabrick extends AbstractTemplateAreabrick
+{
+    /**
+     * @inheritDoc
+     */
+    public function getTemplateLocation()
+    {
+        return static::TEMPLATE_LOCATION_GLOBAL;
+    }
+
+    public function getHtmlTagOpen(Info $info)
+    {
+        return '';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getHtmlTagClose(Info $info)
+    {
+        return '';
+    }
+}
