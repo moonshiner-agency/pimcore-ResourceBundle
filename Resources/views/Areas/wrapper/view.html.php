8908b05f48ed4a8e35da6589610a99951f2fce81
Fabian Hippmann <fabian.hippmann@moonshiner.at>
2019-07-05 11:06:32 +0200

    add wrapper layout option

--- Resources/views/Areas/wrapper/view.html.php
+++ Resources/views/Areas/wrapper/view.html.php
@@ -18,7 +18,6 @@
  * @var \Pimcore\Templating\PhpEngine $view
  * @var \Pimcore\Templating\GlobalVariables $app
  */
-
 $id = 'areaBlock';
 ?>
 
@@ -27,15 +26,32 @@ $id = 'areaBlock';
     ?>
     <section>
         <div class="cms-component-type">Wrapper</div>
+        <br>
+        <strong>Theme</strong>
+        <br>
         <?= $this->select('theme', [
-            'reload' => true,
             'store' => [
+                ['default', 'Default'],
                 ['primary', 'Primary'],
                 ['secondary', 'Secondary']
             ]
         ]); ?>
-        <?= $this->checkbox("fullWidth", ["label" => "Full Width?"]); ?>
-
+        <br>
+        <strong>Layout</strong>
+        <?= $this->select('layout', [
+            'reload' => true,
+            'store' => [
+                ['one', 'Default'],
+                ['2x2', '2x2'],
+                ['1-2x2', '1 - 2 - 2'],
+                ['2x2-1', '2 - 2 - 1']
+            ]
+        ]); ?>
+        <br>
+        <br>
+         <strong>Width</strong><br>
+        <?= $this->checkbox('fullWidth', ['label' => 'Full Width?']); ?>
+        <div class="wrapper-subsection-<?= $this->select('layout')->getData() ?>">
         <?= $this->areablock($id, [
             'allowed' => [
                 'gallery-carousel',
@@ -54,53 +70,103 @@ $id = 'areaBlock';
                 'directions',
             ]
         ]) ?>
+        <style>
+        .wrapper-subsection-2x2 .pimcore_tag_areablock {
+                display: grid;
+                grid-gap: 20px;
+                grid-template-columns: 50% 50%;
+            }
+            .wrapper-subsection-2x2 .pimcore_tag_areablock .pimcore_area_entry section {
+    height: 100%;
+    margin: 0;
+}
+
+.wrapper-subsection-2x2-1 .pimcore_tag_areablock {
+                grid-gap: 20px;
+  display: grid;
+  align-items: center;
+  grid-template-rows: auto 1fr auto;  /* key rule */
+  grid-template-columns: 50% 50%;
+  grid-template-areas:
+    'a b'
+    'c c';
+
+}
+.wrapper-subsection-2x2-1 .pimcore_area_entry:first-of-type section{
+    margin: 0; 
+    height: 100%;
+
+}
+.wrapper-subsection-2x2-1 .pimcore_area_entry:first-of-type {
+    grid-area: a;
+    height: 100%; 
+  }
+  .wrapper-subsection-2x2-1 .pimcore_area_entry:nth-of-type(2) section{
+    margin: 0; 
+    height: 100%;
+      }
+  .wrapper-subsection-2x2-1 .pimcore_area_entry:nth-of-type(2) {
+    grid-area: b;
+    height: 100%; 
+  }
+  .wrapper-subsection-2x2-1 .pimcore_area_entry:nth-of-type(3) section{
+    margin: 0; 
+    height: 100%;
+      }
+  .wrapper-subsection-2x2-1 .pimcore_area_entry:nth-of-type(3) {
+    margin: 0; 
+    grid-area: c;
+    height: 100%; 
+  }
+        </style>
+        </div>
     </section>
 <?php
 } else {
-    $areaBlock = $this->areablock($id, [
-        "manual" => true,
-        'allowed' => [
-            'wrapper',
-            'gallery-carousel',
-            'wysiwyg',
-            'highlight',
-            'relatable',
-            'hero',
-            'image',
-            'horizontal-line',
-            'icon-teaser-row',
-            'contact-card',
-            'productteaser',
-            'card',
-            'section-title',
-            'testimonial',
-            'wrapper',
-            'directions',
-        ]
-    ])->start();
+            $areaBlock = $this->areablock($id, [
+                'manual' => true,
+                'allowed' => [
+                    'wrapper',
+                    'gallery-carousel',
+                    'wysiwyg',
+                    'highlight',
+                    'relatable',
+                    'hero',
+                    'image',
+                    'horizontal-line',
+                    'icon-teaser-row',
+                    'contact-card',
+                    'productteaser',
+                    'card',
+                    'section-title',
+                    'testimonial',
+                    'wrapper',
+                    'directions',
+                ]
+            ])->start();
 
-    $componentsCopy = $this->slots()->components;
+            $componentsCopy = $this->slots()->components;
 
-    $this->slots()->components = [];
-    while ($areaBlock->loop()) {
-        $areaBlock->blockConstruct();
-        $areaBlock->blockStart();
-        $areaBlock->content();
-        $areaBlock->blockEnd();
-        $areaBlock->blockDestruct();
-    }
+            $this->slots()->components = [];
+            while ($areaBlock->loop()) {
+                $areaBlock->blockConstruct();
+                $areaBlock->blockStart();
+                $areaBlock->content();
+                $areaBlock->blockEnd();
+                $areaBlock->blockDestruct();
+            }
 
-    $areaBlock->end();
+            $areaBlock->end();
 
-    $elements = $this->slots()->components;
+            $elements = $this->slots()->components;
 
+            $this->slots()->components = $componentsCopy;
 
-    $this->slots()->components = $componentsCopy;
-
-    $this->slots()->components[] = [
-        'type' => 'wrapper',
-        'isFullWidth' => $this->checkbox("fullWidth")->isChecked(),
-        'theme' => $this->select('theme')->getData(),
-        'data' => $elements
-    ];
-} ?>
+            $this->slots()->components[] = [
+                'type' => 'wrapper',
+                'isFullWidth' => $this->checkbox('fullWidth')->isChecked(),
+                'theme' => $this->select('theme')->getData(),
+                'layout' => $this->select('layout')->getData(),
+                'data' => $elements
+            ];
+        } ?>
