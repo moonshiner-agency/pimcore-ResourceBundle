bbd6276940cc4844f196686ba51a4f2b830524a2
raphael.fleischmann@moonshiner.at <raphael.fleischmann@moonshiner.at>
2019-09-03 15:31:36 +0200

    refactored getUri of Navigation, created LinkResource

---
+++ JsonResources/LinkResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Moonshiner\BrigthenBundle\JsonResources;
+
+use Moonshiner\BrigthenBundle\Services\Navigation;
+use Moonshiner\BrigthenBundle\Services\Resource;
+
+class LinkResource extends Resource
+{
+    public function toArray()
+    {
+        $link = $this->resource->getData();
+
+        return [
+            'internalType' => $link['internalType'],
+            'linktype' => $link['linktype'],
+            'text' => $link['text'],
+            'path' => Navigation::getUri($link['path']),
+            'target' => $link['target'],
+            'parameters' => $link['parameters'],
+            'anchor' => $link['anchor'],
+            'title' => $link['title'],
+            'accesskey' => $link['accesskey'],
+            'rel' => $link['rel'],
+            'tabindex' => $link['tabindex'],
+            'class' => $link['class'],
+            'attributes' => $link['attributes'],
+            'internal' => $link['internal'],
+            'internalId' => $link['internalId'],
+        ];
+    }
+}
