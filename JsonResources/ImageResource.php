a60bf26b08ad5ea5388559961383e0bdc061cbf9
Raphael Fleischmann <raphael.fleischmann@moonshiner.at>
2019-11-21 17:19:29 +0100

    add thumbnails to image resource

--- JsonResources/ImageResource.php
+++ JsonResources/ImageResource.php
@@ -9,19 +9,17 @@ class ImageResource extends Resource
 {
     public function toArray()
     {
-        $host =  SystemSettings::getHostUrl();
+        $host = SystemSettings::getHostUrl();
+
         return [
             'src' => $host . $this->resource->getFullPath(),
             'thumbnails' => [
-                'intended_lg' => $host . $this->resource->getThumbnail('intended_lg')->getPath(),
-                'intended_md' => $host . $this->resource->getThumbnail('intended_md')->getPath(),
-                'intended_sm' => $host . $this->resource->getThumbnail('intended_sm')->getPath(),
-                'thumbnail_md' => $host . $this->resource->getThumbnail('thumbnail_md')->getPath(),
-                'thumbnail_sm' => $host . $this->resource->getThumbnail('thumbnail_sm')->getPath(),
-                'mobile_intended_md' => $host . $this->resource->getThumbnail('mobiles_intended_md')->getPath(),
-                'mobile_intended_sm' => $host . $this->resource->getThumbnail('mobile_intended_sm')->getPath(),
-                'mobile_thumbnail_md' => $host . $this->resource->getThumbnail('mobile_thumbnail_md')->getPath(),
-                'mobile_thumbnail_sm' => $host . $this->resource->getThumbnail('mobile_thumbnail_sm')->getPath(),
+                'cover' => $host . $this->resource->getThumbnail('cover')->getPath(),
+                'card' => $host . $this->resource->getThumbnail('card')->getPath(),
+                'smallCard' => $host . $this->resource->getThumbnail('smallCard')->getPath(),
+                'highlightedItem' => $host . $this->resource->getThumbnail('highlightedItem')->getPath(),
+                'callToAction' => $host . $this->resource->getThumbnail('callToAction')->getPath(),
+                'icon' => $host . $this->resource->getThumbnail('icon')->getPath(),
             ]
         ];
     }
